580ddc6f712c1292b0767b9e54cdaffc0cb2c5c7
Intisar Sikder <sikder.intisar@gmail.com>
2023-04-05 00:05:02 +0100

    first commit

---
+++ src/main/java/Media.java
@@ -0,0 +1,61 @@
+public class Media{
+
+    protected String title;
+    protected String publisher;
+    protected String genre;
+    protected int year;
+    protected int rating;
+
+    public Media(String title, String publisher, String genre, int year, int rating) {
+        this.title = title;
+        this.publisher = publisher;
+        this.genre = genre;
+        this.year = year;
+        this.rating = rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String isRecommended(){
+        return "Yes! It's really good.";
+    }
+
+}
